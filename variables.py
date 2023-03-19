20d588f87d455832e4b57aae0dcc800730e48d2d
Swapnali Thakur <68456440+SwapTH@users.noreply.github.com>
2023-03-19 16:42:46 +0530

    commit python Basic

---
+++ variables.py
@@ -0,0 +1,21 @@
+#Python Variables
+#Variables are containers for storing data values.
+#A variable is created the moment you first assign a value to it.
+#
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
+sum = num_1 + num_2
+print("Sum of two numbers is",sum)
+
+diff = num_1 - num_2
+print("Difference of two numbers is",diff)
+
+product = num_1 * num_2
+print("Product of two numbers is",product)
+
+quotient = num_1 / num_2
+print("Quotient of two numbers is",quotient)
+
+remainder = num_1 % num_2
+print("Remainder of two numbers is",remainder)
